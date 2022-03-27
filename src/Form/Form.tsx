bbaf8e6f85ebf1f26a30e8c5ae682f2e12c0e132
victorduclos <victor.duclos@protonmail.com>
2022-03-27 13:08:30 +0200

    feat: implements `form` in `Form` and use `onSubmit`

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,15 +1,23 @@
-import React, { ReactNode } from 'react';
+import React, {
+  FormHTMLAttributes,
+  ReactNode,
+  useCallback,
+} from 'react';
+import {
+  FieldValues,
+  mapValidationStatusesToOutcome,
+  VALIDATION_OUTCOME,
+} from '../shared';
 import { FormContext, FormContextApi } from './contexts/FormContext';
 
-interface FormProps {
+interface FormProps extends Omit<FormHTMLAttributes<HTMLFormElement>, 'onSubmit'> {
   children: ReactNode,
   form: FormContextApi,
+  onSubmit?: (formValues: FieldValues) => void | Promise<void>,
 }
 
 /**
  * Context to handle Form. Similar to <form> but using React Context
- * @param children - Children
- * @param form - Form from useForm hook
  * @example
  * ```
  * const form = useForm():
@@ -21,10 +29,33 @@ interface FormProps {
  * )
  * ```
  */
-export function Form({ children, form }: FormProps) {
+export function Form({
+  children,
+  form,
+  onSubmit,
+  ...otherProps
+}: FormProps) {
+  const {
+    getFormValues,
+    formInternal: {
+      getFormErrorsForNames,
+    },
+  } = form;
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = useCallback((event) => {
+    event.preventDefault();
+    if (onSubmit) {
+      const validations = getFormErrorsForNames();
+      const isFormValid = mapValidationStatusesToOutcome(validations) === VALIDATION_OUTCOME.VALID;
+      if (isFormValid) onSubmit(getFormValues());
+    }
+  }, [getFormErrorsForNames, getFormValues, onSubmit]);
+
   return (
     <FormContext.Provider value={form}>
-      {children}
+      <form {...otherProps} onSubmit={handleSubmit}>
+        {children}
+      </form>
     </FormContext.Provider>
   );
 }
