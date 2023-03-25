fdaba2ba541d60d88836944390c2acfd2baf7af8
victorduclos <74572718+victorduclos@users.noreply.github.com>
2023-03-25 22:53:27 +0100

    feat: export `FormContextApi` and `UseFormOptions` types (#39)

--- src/form/contexts/RuleContext.ts
+++ src/form/contexts/RuleContext.ts
@@ -2,8 +2,8 @@ import { createContext, useContext } from 'react';
 import { Validator } from '../types/Validation';
 
 export interface RuleContextApi {
-  registerRule: (testFn: Validator, errorMessage: string, isDebounced: boolean) => void;
-  unregisterRule: (testFn: Validator, isDebounced: boolean) => void;
+  registerRule(testFn: Validator, errorMessage: string, isDebounced: boolean): void;
+  unregisterRule(testFn: Validator, isDebounced: boolean): void;
 }
 
 export const CONTEXT_RULE_DEFAULT: RuleContextApi = {
