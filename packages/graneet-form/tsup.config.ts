e4d6e1081404caf3445819d0b7d392805478beb1
Victor <74572718+victorduclos@users.noreply.github.com>
2025-05-12 22:39:42 +0200

    feat!: change target to es2020 and migrate to esm only (#106)

--- packages/graneet-form/tsup.config.ts
+++ packages/graneet-form/tsup.config.ts
@@ -4,8 +4,8 @@ const isProduction = (options: Options) => options.env?.NODE_ENV === 'production
 
 export default defineConfig((options) => ({
   entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
-  target: 'es6',
+  format: ['esm'],
+  target: 'es2020',
   clean: true,
   splitting: true,
   treeshake: true,
