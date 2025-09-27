1cc8bb35da3f0d0c44fbd22b2c07642b0c96095f
victorduclos <victor@graneet.fr>
2025-09-27 22:54:49 +0200

    feat: migrate `apps/docs` to `apps/docs-legacy` directory structure

---
+++ apps/docs/src/lib/source.ts
@@ -0,0 +1,25 @@
+import { docs } from '@/.source';
+import { type InferPageType, loader } from 'fumadocs-core/source';
+
+// See https://fumadocs.vercel.app/docs/headless/source-api for more info
+export const source = loader({
+  baseUrl: '/docs',
+  source: docs.toFumadocsSource(),
+});
+
+export function getPageImage(page: InferPageType<typeof source>) {
+  const segments = [...page.slugs, 'image.png'];
+
+  return {
+    segments,
+    url: `/og/docs/${segments.join('/')}`,
+  };
+}
+
+export async function getLLMText(page: InferPageType<typeof source>) {
+  const processed = await page.data.getText('processed');
+
+  return `# ${page.data.title} (${page.url})
+
+${processed}`;
+}
