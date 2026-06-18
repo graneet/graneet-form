19c67abd4785394337517e85eac4d5bea7fb4124
victorduclos <victor@graneet.fr>
2026-06-18 12:47:20 +0200

    feat: tsdown

--- packages/graneet-form/tsdown.config.ts
+++ packages/graneet-form/tsdown.config.ts
@@ -1,19 +1,21 @@
 // oxlint-disable-next-line import/no-nodejs-modules
 import { copyFileSync } from 'node:fs';
 import { defineConfig } from 'tsdown';
-import type { Options } from 'tsdown';
+import type { InlineConfig } from 'tsdown';
 
-const isProduction = (options: Options) => options.env?.['NODE_ENV'] === 'production';
+const isProduction = (options: InlineConfig) => options.env?.['NODE_ENV'] === 'production';
 
 export default defineConfig((options) => ({
   clean: true,
   dts: true,
   entry: ['src/index.ts'],
+  exports: true,
   format: ['esm'],
   minify: isProduction(options),
   onSuccess: () => {
     copyFileSync('../../README.md', 'README.md');
   },
+  publint: true,
   sourcemap: !isProduction(options),
   splitting: true,
   target: 'es2020',
